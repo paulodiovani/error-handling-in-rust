117a7e1b3e245587736e67f45f347543aa464462
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-07-22 17:46:10 -0300

    Use &str and loop through files

--- src/file_contents.rs
+++ src/file_contents.rs
@@ -1,10 +1,9 @@
-
 use crate::errors::FileError;
-use std::{fs::File, io::Read};
+use std::{fs::File, io::{ErrorKind, Read}};
 
 /// Reads a file and unwrap or panic!
 #[allow(dead_code)]
-pub fn read_file_and_unwrap(filename: String) -> Result<String, ()> {
+pub fn read_file_and_unwrap(filename: &str) -> Result<String, ()> {
     let mut file = File::open(filename).unwrap();
 
     let mut content = String::new();
@@ -17,9 +16,35 @@ pub fn read_file_and_unwrap(filename: String) -> Result<String, ()> {
     Ok(content)
 }
 
+/// Reads a file and returns content or hardcoded error strings
+#[allow(dead_code)]
+pub fn read_file_or_return_message(filename: &str) -> Result<String, &'static str> {
+    let file = File::open(filename);
+    match file {
+        Ok(mut file) => {
+            let mut content = String::new();
+            match file.read_to_string(&mut content) {
+                Ok(_) => {
+                    if content.is_empty() {
+                        Err("File is empty")
+                    } else {
+                        Ok(content)
+                    }
+                }
+                Err(_) => Err("Unknown error"),
+            }
+        }
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => Err("File not found"),
+            ErrorKind::PermissionDenied => Err("Missing permissions to read file"),
+            _ => Err("Unknown error"),
+        },
+    }
+}
+
 /// Reads a file and print contents of error message (the verbose way)
 #[allow(dead_code)]
-pub fn read_file_and_match(filename: String) -> Result<String, FileError> {
+pub fn read_file_and_match(filename: &str) -> Result<String, FileError> {
     let file = File::open(filename);
 
     match file {
@@ -42,7 +67,7 @@ pub fn read_file_and_match(filename: String) -> Result<String, FileError> {
 
 /// Final: Reads a file and print contents or error message
 #[allow(dead_code)]
-pub fn read_file(filename: String) -> Result<String, FileError> {
+pub fn read_file(filename: &str) -> Result<String, FileError> {
     let mut file = File::open(filename)?;
     let mut content = String::new();
 
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ mod file_contents;
 use crate::file_contents::*;
 
 fn main() {
-    let filename = String::from("sample-hello.txt");
-    // let filename = String::from("sample-empty.txt");
-    // let filename = String::from("sample-not-found.txt");
-    // let filename = String::from("sample-forbidden.txt");
+    let filenames = vec![
+        "sample-hello.txt",
+        "sample-empty.txt",
+        "sample-not-found.txt",
+        "sample-forbidden.txt",
+    ];
 
-    // let file_contents = read_file_and_unwrap(filename);
-    // let file_contents = read_file_and_match(filename);
-    let file_contents = read_file(filename);
+    for filename in filenames {
+        let file_contents = read_file_and_unwrap(filename);
+        // let file_contents = read_file_or_return_message(filename);
+        // let file_contents = read_file_and_match(filename);
+        // let file_contents = read_file(filename);
 
-    match file_contents {
-        Ok(contents) => println!("File contents: {}", contents),
-        Err(err) => println!("Error reading file: {:?}", err),
+        match file_contents {
+            Ok(contents) => println!("File contents: {}", contents),
+            Err(err) => println!("Error reading file: {:?}", err),
+        }
     }
+
 }
